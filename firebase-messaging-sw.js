db25de34b342546bd64fc5eacae0dc52e6453c71
AcertosOnline <acertosonlinejogodobicho@gmail.com>
2025-03-07 18:57:32 -0300

    Update and rename firebase-messaging-sw.js.js to firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyA1C3iYQe22zhTP5HVj19atOZLROtba3rw",
+    authDomain: "jogo-do-bicho-421ff.firebaseapp.com",
+    databaseURL: "https://jogo-do-bicho-421ff-default-rtdb.firebaseio.com",
+    projectId: "jogo-do-bicho-421ff",
+    storageBucket: "jogo-do-bicho-421ff.firebasestorage.app",
+    messagingSenderId: "1023919123583",
+    appId: "1:1023919123583:web:b6c561fb121fe54f9e234a",
+    measurementId: "G-BPH150V2SG"
+});
+
+const messaging = firebase.messaging();
